3f9cee50dd8320d69e938c3076f8db8b0759a534
Mello Nite <mellooo@mellos-mbp.elon.edu>
2018-11-02 16:53:44 -0400

    Add files

--- Labyrinth_Nov/Assets/Scripts/Player_SCRIPTS/PlayerPowers.cs
+++ Labyrinth_Nov/Assets/Scripts/Player_SCRIPTS/PlayerPowers.cs
@@ -7,19 +7,83 @@ using UnityEngine.SceneManagement;
 public class PlayerPowers : MonoBehaviour
 {
 
+    Dictionary<string, Del> dict = new Dictionary<string, Del>();
     public string powerName = "";
+    public delegate void Del();
+    private int trapsLeft = 5;
+    private float speedBoost = 50f;
+    private GameObject trap;
+    public Text trapText;
+    public GameObject trapPrefab;
+    [SerializeField] private PlayerMovement player;
 
+    void Awake()
+    {
+        MakeDelegates();
+    }
 
     void Update()
     {
         GetUniqueAbility();
     }
 
-
-    public void SetPowers(string theName){
-        powerName = theName;
+    public void GetUniqueAbility()
+    {
+        Del power;
+        if (dict.TryGetValue(powerName, out power))
+        {
+            power();
+        }
     }
 
-    public virtual void GetUniqueAbility() { }
+    public void Artemis()
+    {
+        if (powerName == "artemis")
+        {
+            trapText.text = "Traps: " + trapsLeft;
+            if (Input.GetKeyDown("o") && trapsLeft > 0)
+            {
+                trap = Instantiate(trapPrefab) as GameObject;
+                trap.transform.position = this.transform.TransformPoint(0, -1.1f, 4);
+                trapsLeft--;
+                Destroy(trap, 15);
+            }
+        }
+    }
+    public void Hermes()
+    {
+        if (powerName == "hermes")
+        {
+            player.SetHermesSpeed(speedBoost);
+        }
+    }
+    public void Hates()
+    {
+        if (powerName == "hates")
+        {
+            Debug.Log("Getting power");
+        }
+    }
+    public void Poseidon()
+    {
+        if (powerName == "poseidon")
+        {
+            Debug.Log("Getting power");
+        }
+    }
 
+    public void SetPowers(string theName){
+        this.powerName = theName;
+    }
+
+    public void MakeDelegates(){
+        Del artemis = Artemis;
+        Del hates = Hates;
+        Del hermes = Hermes;
+        Del poseidon = Poseidon;
+        dict.Add("artemis", artemis);
+        dict.Add("hates", hates);
+        dict.Add("hermes", hermes);
+        dict.Add("poseidon", poseidon);
+    }
 }
--- Labyrinth_Nov/Assets/Scripts/PowerUp_SCRIPTS/Hermes.cs
+++ Labyrinth_Nov/Assets/Scripts/PowerUp_SCRIPTS/Hermes.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hermes : PowerUpsDisplay {
-    private float speedBoost = 50f;
-    [SerializeField] private PlayerMovement moveObject;
-    [SerializeField] private GameObject artmeis;
-    //[SerializeField] private GameObject _____;
-    //[SerializeField] private GameObject _____;
 
+    [SerializeField] private GameObject artmeis;
+    [SerializeField] private PlayerPowers power;
+    //[SerializeField] private GameObject _____;
+    //[SerializeField] private GameObject _____;
 
     public override void ApplyPowerUp(){
-        moveObject.SetHermesSpeed(speedBoost);
+        power.SetPowers("hermes");
     }
     public override void DestroyOtherPowerUps() {
         Destroy(artmeis);
